ea68dd534a39ad7bd6ddbe4628be93144bb4d750
pramudithasadeepa <mlpramuditha1@example.com>
2026-01-31 11:24:23 +0530

    Introduce quality issues 4

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,18 @@
-package main.java.com.example;
+package com.example;
+
+import java.util.logging.Logger;
 
 public class App {
-public static void main(String[] args) throws Exception { 
-Calculator calc = new Calculator(); 
-System.out.println(calc.calculate(10, 5, "add-again")); 
-UserService service = new UserService(); 
-service.findUser("admin"); 
-service.deleteUser("admin"); // NEW dangerous call 
-} 
 
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+        Calculator calc = new Calculator();
+        logger.info("Result: " + calc.calculate(10, 5, "add-again"));
+
+        UserService service = new UserService();
+        service.findUser("admin");
+        service.deleteUser("admin"); // NEW dangerous call
+    }
 }
-
